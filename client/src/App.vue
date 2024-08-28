e3ea8f5649449a762e8825cdc19c10565415e113
LaithSM01 <laithsamhan1@gmail.com>
2024-08-28 22:15:28 +0300

    section 7 client setup, first http req

---
+++ client/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <app-nav-bar></app-nav-bar>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import navBar from "./components/nav-bar"
+export default {
+  components: {
+    appNavBar: navBar
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
